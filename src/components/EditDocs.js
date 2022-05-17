332967d55f8fac2612b593472b80565ca902be98
Artem Khasin <akhasin.dev@gmail.com>
2022-05-17 16:00:36 +0300

    edict docs func

--- src/components/EditDocs.js
+++ src/components/EditDocs.js
@@ -1,18 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-
-const EditDocs = () => {
-
+import {
+    updateDoc,
+    collection,
+    doc,
+    onSnapshot
+} from 'firebase/firestore';
+export default function EditDocs({
+    database
+}) {
+    const isMounted = useRef()
+    const collectionRef = collection(database, 'docsData')
     let params = useParams();
+    const [documentTitle, setDocumentTitle] = useState('')
+    const [docsDesc, setDocsDesc] = useState('');
+    const getQuillData = (value) => {
+        setDocsDesc(value)
+    }
+    useEffect(() => {
+        const updateDocsData = setTimeout(() => {
+            const document = doc(collectionRef, params.id)
+            updateDoc(document, {
+                docsDesc: docsDesc
+            })
+            .catch(() => {
+                    alert('Cannot Save')
+                })
+        }, 1000)
+        return () => clearTimeout(updateDocsData)
+        // eslint-disable-next-line
+    }, [docsDesc])
 
-  return (
-    <div>
-        <h1>Edit Docs</h1>
-        <ReactQuill/>
-    </div>
-  )
+    const getData = () => {
+        const document = doc(collectionRef, params.id)
+        onSnapshot(document, (docs) => {
+            setDocumentTitle(docs.data().title)
+            setDocsDesc(docs.data().docsDesc);
+        })
+    }
+
+    useEffect(() => {
+        if (isMounted.current) {
+            return
+        }
+
+        isMounted.current = true;
+        getData()
+        // eslint-disable-next-line
+    }, [])
+    return (
+        <div className='editDocs-main'>
+            <h1>{documentTitle}</h1>
+            <div className='editDocs-inner'>
+                <ReactQuill
+                    className='react-quill'     
+                    value={docsDesc}
+                    onChange={getQuillData}
+                />
+            </div>
+
+        </div>
+    )
 }
-
-export default EditDocs;
